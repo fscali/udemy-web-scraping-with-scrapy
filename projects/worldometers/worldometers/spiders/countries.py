bd3dfcbc7ebd6f2e5758334f5582465533538f5d
fscali <francesco.scali@gmail.com>
2022-03-14 15:54:01 +0100

    following link with relative links

--- projects/worldometers/worldometers/spiders/countries.py
+++ projects/worldometers/worldometers/spiders/countries.py
@@ -15,5 +15,5 @@ class CountriesSpider(scrapy.Spider):
             link = country.xpath(".//@href").get()
 
             #absolute_url = f'https://www.worldometers.info/{link}'
-            absolute_url = response.urljoin(link)
-            yield scrapy.Request(url=absolute_url)
+            #absolute_url = response.urljoin(link)
+            yield response.follow(url=link)
