e99252ceaa4334030af914e6a4dbf94d716e63b0
fscali <francesco.scali@gmail.com>
2022-03-14 15:53:13 +0100

    following links with absolute urls - method1

--- projects/worldometers/worldometers/spiders/countries.py
+++ projects/worldometers/worldometers/spiders/countries.py
@@ -4,7 +4,7 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = [
         'https://www.worldometers.info/world-population/population-by-country/']
 
@@ -14,7 +14,6 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield {
-                'country_name': name,
-                'country_link': link
-            }
+            absolute_url = f'https://www.worldometers.info/{link}'
+            #absolute_url = response.urljoin(link)
+            yield scrapy.Request(url=absolute_url)
