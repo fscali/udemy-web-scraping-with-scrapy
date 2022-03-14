5abf9c85aa72a5b729a2ea424cdac7952a46bd03
fscali <francesco.scali@gmail.com>
2022-03-14 16:01:15 +0100

    dealing with responses from following links

--- projects/worldometers/worldometers/spiders/countries.py
+++ projects/worldometers/worldometers/spiders/countries.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -16,4 +17,16 @@ class CountriesSpider(scrapy.Spider):
 
             #absolute_url = f'https://www.worldometers.info/{link}'
             #absolute_url = response.urljoin(link)
-            yield response.follow(url=link)
+            yield response.follow(url=link, callback=self.parse_country)
+
+    def parse_country(self, response):
+        rows = response.xpath(
+            "(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield {
+                'year': year,
+                'population': population
+            }
