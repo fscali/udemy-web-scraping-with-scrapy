148ba2eb6529c0ec0fb0e4a29e0eb0c15862fb27
fscali <francesco.scali@gmail.com>
2022-03-14 16:05:23 +0100

    passing info from parent parser to child parser

--- projects/worldometers/worldometers/spiders/countries.py
+++ projects/worldometers/worldometers/spiders/countries.py
@@ -17,9 +17,10 @@ class CountriesSpider(scrapy.Spider):
 
             #absolute_url = f'https://www.worldometers.info/{link}'
             #absolute_url = response.urljoin(link)
-            yield response.follow(url=link, callback=self.parse_country)
+            yield response.follow(url=link, callback=self.parse_country, meta={'country': name})
 
     def parse_country(self, response):
+        name = response.request.meta.get('country')
         rows = response.xpath(
             "(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
 
@@ -27,6 +28,7 @@ class CountriesSpider(scrapy.Spider):
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/strong/text()").get()
             yield {
+                'country': name,
                 'year': year,
                 'population': population
             }
