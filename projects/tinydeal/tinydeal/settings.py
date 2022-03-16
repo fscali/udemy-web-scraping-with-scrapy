0bd060947d85b47b0a0b7cdd7a627a981a8fe50f
fscali <francesco.scali@gmail.com>
2022-03-16 08:12:32 +0100

    overriding user agent - method 1 (but not flexible if we want to override other headers)

--- projects/tinydeal/tinydeal/settings.py
+++ projects/tinydeal/tinydeal/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'tinydeal.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tinydeal (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
