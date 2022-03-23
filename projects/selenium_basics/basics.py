5e1dfffdb18b9a06c10898ef00042c90609303b2
fscali <francesco.scali@gmail.com>
2022-03-23 08:24:42 +0100

    chrome headless

--- projects/selenium_basics/basics.py
+++ projects/selenium_basics/basics.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from shutil import which
 
 #chrome_path = which("chromedriver")
 
-driver = webdriver.Chrome(executable_path="./chromedriver")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(
+    executable_path="./chromedriver", options=chrome_options)
 driver.get("https://duckduckgo.com")
 search_input = driver.find_element(By.XPATH,
                                    "(//input[contains(@class,'js-search-input')])[1]")
@@ -16,3 +21,6 @@ search_input.send_keys("my user agent")
 search_btn.click()
  """
 search_input.send_keys(Keys.ENTER)
+print(driver.page_source)
+
+driver.close()
